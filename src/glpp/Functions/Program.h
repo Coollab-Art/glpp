664f394d79f0cda1b4b8f18311e2baa9e5a5109f
Jules Fouchy <jules.fouchy@ntymail.com>
2022-04-09 11:45:02 +0200

    [Program] Tweak documentation

--- src/glpp/Functions/Program.h
+++ src/glpp/Functions/Program.h
@@ -10,7 +10,8 @@ public:
     void attach_shader(GLuint shader_id) const;
     void link() const;
     /// This is a function to debug the state of your shader
-    /// that you can call just before a draw call to make sure that uniforms don't have weird values and a few other things.
+    /// that you can call just before a draw call to make sure that uniforms have been properly set and a few other things.
+    /// https://community.khronos.org/t/samplers-of-different-types-use-the-same-textur/66329/4
     MaybeError check_for_state_errors() const;
 
     GLuint operator*() const { return *id_; }
